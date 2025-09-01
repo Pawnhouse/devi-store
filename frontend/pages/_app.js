241ee2bf2563479d999a9bdb716cf837fd731a4f
Eugene <ibayak@gmail.com>
2025-09-01 17:22:15 +0300

    use Roboto light font

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
 import Header from "../components/Header";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "../theme";
 
 export default function MyApp({ Component, pageProps }) {
     return (
-        <div className="container">
-            <Header/>
-            <Component {...pageProps} />
-        </div>
+        <>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <div className="container">
+                    <Header/>
+                    <Component {...pageProps} />
+                </div>
+            </ThemeProvider>
+        </>
     );
 }
---
+++ frontend/theme.js
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    typography: {
+        fontFamily: "'Roboto', Arial, sans-serif",
+        body1: {
+            fontWeight: 300
+        },
+        h1: {
+            fontWeight: 400
+        },
+        h2: {
+            fontWeight: 400
+        },
+        h3: {
+            fontWeight: 400
+        },
+        button: {
+            fontWeight: 300
+        },
+    }
+});
