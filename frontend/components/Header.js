1d032647ba9c816a6fd7edc5eb8844241bceedf6
Eugene <ibayak@gmail.com>
2025-09-17 14:10:55 +0300

    add logo

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,11 +1,20 @@
 import Link from 'next/link';
-import Image from 'next/image'
+import Image from 'next/image';
 import NavigationMenu from "./NavigationMenu";
+import Logo from '../public/images/logo.svg';
 
 export default function Header() {
     return (
-        <header style={{padding: '1rem 0', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <header style={{ padding: '1rem 0', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <NavigationMenu />
+            <Link href="/">
+                <Image
+                    className="logo"
+                    alt="Devi Logo"
+                    src={Logo}
+                    priority
+                />
+            </Link>
             <nav>
                 <Link href="/cart">
                     <div className="icon-container">
